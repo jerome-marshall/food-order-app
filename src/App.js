70a2a009cb71c70f257ed053a48134a99d8fff8c
Jerome Marshall <jeromemarshall0@gmail.com>
2021-05-13 10:36:23 +0530

    Added layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Header from "./components/Layout/Header";
+import {Fragment} from "react";
+import Meals from "./components/Meals/Meals";
+
 function App() {
-  return (
-    <div>
-      <h2>Hi</h2>
-    </div>
-  );
+    return (
+        <Fragment>
+            <Header/>
+            <main>
+                <Meals/>
+            </main>
+        </Fragment>
+    );
 }
 
 export default App;
